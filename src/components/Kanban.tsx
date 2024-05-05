e8dee8084af471a46afb0bdc56dda8cbd6671b3f
tomy08 <tomasesantacruz@gmail.com>
2024-05-04 21:33:28 -0300

    Fix: Update Kanban component to persist cards in local storage

--- src/components/Kanban.tsx
+++ src/components/Kanban.tsx
@@ -1,6 +1,21 @@
+import { useEffect, useContext } from 'react'
+import { CardsContext } from '../context/KanbanContext'
+
 import Column from './SectionDroppable'
 
 export default function Kanban() {
+  const { cards, setCards } = useContext(CardsContext)!
+  useEffect(() => {
+    const data = localStorage.getItem('cards')
+    if (data) {
+      const cards = JSON.parse(data)
+      setCards(cards)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('cards', JSON.stringify(cards))
+  }, [cards])
   return (
     <main className="p-4 w-full min-h-full grid grid-cols-3 gap-8">
       <Column id="todo" title="Pending" bgColor="bg-blue-800" />
--- src/context/KanbanContext.tsx
+++ src/context/KanbanContext.tsx
@@ -1,25 +1,13 @@
-import { useState, createContext, useEffect } from 'react'
+import { useState, createContext } from 'react'
 
 import type { Card, CardsContextType } from '../types'
 
 export const CardsContext = createContext<CardsContextType | null>(null)
 
 export function ContextProvider({ children }: { children: React.ReactNode }) {
-  const [cards, setCards] = useState<Card[]>([])
+  const data = localStorage.getItem('cards')
 
-  useEffect(() => {
-    const data = localStorage.getItem('cards')
-    if (data) {
-      const cards = JSON.parse(data)
-      setCards(cards)
-    } else {
-      localStorage.setItem('cards', JSON.stringify([]))
-    }
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem('cards', JSON.stringify(cards))
-  }, [cards])
+  const [cards, setCards] = useState<Card[]>(data ? JSON.parse(data) : [])
 
   return (
     <CardsContext.Provider value={{ cards, setCards }}>
