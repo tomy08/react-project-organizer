aabfb9d7b7978da3ee8fe3912657ef738435f199
tomy08 <tomasesantacruz@gmail.com>
2024-05-05 21:13:41 -0300

    Add: dots vertical icon in the task card

--- src/components/TaskDraggable.tsx
+++ src/components/TaskDraggable.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from '../types'
+import DotsVertical from './svg/DotsVertical'
 
 type TaskDraggableProps = {
   card: Card
@@ -15,19 +16,24 @@ export default function TaskDraggable({ card }: TaskDraggableProps) {
     <div
       draggable
       onDragStart={handleDragStart}
-      className="w-[95%] bg-slate-800 text-white rounded-md text-left p-2 active:cursor-grabbing"
+      className="w-[95%] bg-slate-800 text-white rounded-md text-left p-2 active:cursor-grabbing relative"
     >
       {title}
-      <div className="flex gap-2 mt-2">
-        {badges.map((badge) => (
-          <span
-            key={badge}
-            className="bg-slate-700 text-white text-xs p-1 rounded-md"
-          >
-            {badge}
-          </span>
-        ))}
-      </div>
+      {badges.length > 0 && (
+        <div className="flex gap-2 mt-2">
+          {badges.map((badge) => (
+            <span
+              key={badge}
+              className="bg-slate-700 text-white text-xs p-1 rounded-md"
+            >
+              {badge}
+            </span>
+          ))}
+        </div>
+      )}
+      <button className="absolute top-0 right-0 mt-2 mr-2">
+        <DotsVertical />
+      </button>
     </div>
   )
 }
